378e6e690fb7640036f8ff508e5f169db6157f04
silas meling <silas meling>
2017-03-24 09:03:55 +0100

    små rettelser :3

--- Washcard.java
+++ Washcard.java
@@ -24,10 +24,6 @@ public class Washcard
         this.washCardBalance = washCardBalance;
         this.password = password; 
     }
-    public Washcard() 
-    {
-        
-    }
 
     public double getBalance() 
     {
